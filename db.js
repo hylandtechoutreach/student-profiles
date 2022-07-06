205d5662b57ca7bd4a0500f7a55458577f81a859
Matt2D3 <matttoodeetoo@gmail.com>
2022-07-06 14:20:54 -0400

    capitalization fixed, other school text field needs work

--- db.js
+++ db.js
@@ -10,15 +10,19 @@ module.exports = {
       first_name: studentObj.first_name,
       last_name: studentObj.last_name,
       grade: studentObj.grade,
+<<<<<<< HEAD
       school: studentObj.school,
       email: studentObj.email,
       id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`,
       status: "active",
+=======
+>>>>>>> 6708b5a (capitalization fixed, other school text field needs work)
       school: studentSchool,
 <<<<<<< HEAD
     })
 =======
       email: studentObj.email,
+      school: studentSchool,
       id_number: `${studentObj.last_name}.${ await module.exports.getLastNameCount(studentObj.last_name)}`
     });
 
@@ -39,6 +43,10 @@ module.exports = {
 	},
 
 	editStudentById: async function(studentId, newStudentObj) {
+    let studentSchool = studentObj.school
+    if (studentObj.school == "other"){
+      studentSchool = studentObj.other_school
+    }
     await Student.findOneAndUpdate({
       _id: studentId
     },
