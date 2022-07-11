64d23c767934ecdaa37169fa5ebda230234763c3
Tyler <tdiloreto929@gmail.com>
2022-07-11 15:10:06 -0400

    Now always alphabetical, can now search for name

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,10 @@ module.exports = {
 			ignorePunctuation: true
 		}));
 
+		activeStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
+			ignorePunctuation: true
+		}));
+
 		let renderData = {
 			path: 'none',
 			students: activeStudents
@@ -68,106 +72,6 @@ module.exports = {
 		filteredStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
 			ignorePunctuation: true
 		}));
-
-		let renderData = {
-			path: filteredGrade,
-			students: filteredStudents
-		}
-
-		response.render('index', renderData);
-		
-	},
-
-	sortFirstNames: async function(request, response) {
-		let studentList = await db.getStudentsList();
-		let activeStudents = [];
-		for (let i = 0; i < studentList.length; i++) {
-			if (studentList[i].status == "active") {
-				activeStudents.push(studentList[i]);
-			}
-		}
-
-		activeStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
-			ignorePunctuation: true
-		}));
-
-		let renderData = {
-			path: 'none',
-			students: activeStudents
-		}
-		
-		response.render('index', renderData);
-	},
-
-	filter: async function (request, response) {
-		let studentList = await db.getStudentsList();
-		let filteredGrade = request.params.grade;
-
-		let activeStudents = [];
-		for (let i = 0; i < studentList.length; i++) {
-			if (studentList[i].status == "active") {
-				activeStudents.push(studentList[i]);
-			}
-		}
-
-		let filteredStudents = [];
-		for (let i = 0; i < activeStudents.length; i++) {
-			if (activeStudents[i].grade == filteredGrade) {
-				filteredStudents.push(activeStudents[i]);
-			}
-		}
-
-		filteredStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
-			ignorePunctuation: true
-		}));
-
-		let renderData = {
-			path: filteredGrade,
-			students: filteredStudents
-		}
-
-		response.render('index', renderData);
-		
-	},
-
-	sortFirstNames: async function(request, response) {
-		let studentList = await db.getStudentsList();
-		let activeStudents = [];
-		for (let i = 0; i < studentList.length; i++) {
-			if (studentList[i].status == "active") {
-				activeStudents.push(studentList[i]);
-			}
-		}
-
-		activeStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
-			ignorePunctuation: true
-		}));
-
-		let renderData = {
-			path: 'none',
-			students: activeStudents
-		}
-		
-		response.render('index', renderData);
-	},
-
-	filter: async function (request, response) {
-		let studentList = await db.getStudentsList();
-		let filteredGrade = request.params.grade;
-
-		let activeStudents = [];
-		for (let i = 0; i < studentList.length; i++) {
-			if (studentList[i].status == "active") {
-				activeStudents.push(studentList[i]);
-			}
-		}
-
-		let filteredStudents = [];
-		for (let i = 0; i < activeStudents.length; i++) {
-			if (activeStudents[i].grade == filteredGrade) {
-				filteredStudents.push(activeStudents[i]);
-			}
-		}
 
 		let renderData = {
 			path: filteredGrade,
