8ac0c6c8a14cc03c87bf1b91c30a03a28a908a91
Tyler <tdiloreto929@gmail.com>
2022-07-12 15:57:52 -0400

    Finished tutorial & added pages, not fully working

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,23 +1,28 @@
-const { authJwt } = require('../middleware') //Might be middlewares, not sure
-const controller = require('../controllers/user.controller');
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+
 module.exports = function(app) {
-    app.use(function(req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
-    app.get("/api/test/all", controller.allAccess);
-    app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
-    app.get(
-        "/api/test/mod",
-        [authJwt.verifyToken, authJwt.isModerator],
-        controller.moderatorBoar
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
     );
-    app.get(
-        "/api/test/admin",
-        [authJwt.verifyToken, authJwt.isAdmin],
-        controller.adminBoard
-        );
+    next();
+  });
+
+  app.get("/api/test/all", controller.allAccess);
+
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
+
+  app.get(
+    "/api/test/mod",
+    [authJwt.verifyToken, authJwt.isModerator],
+    controller.moderatorBoard
+  );
+
+  app.get(
+    "/api/test/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
 };
