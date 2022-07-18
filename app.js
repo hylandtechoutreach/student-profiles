b3c7726e9edd5640d3f38373f32d927a3e313c86
Tyler <tdiloreto929@gmail.com>
2022-07-18 15:29:37 -0400

    Changed roles to fit ours, simplified roles code

--- app.js
+++ app.js
@@ -31,12 +31,8 @@ const dbSettings = {
 mongoose.connect(dbStr, dbSettings)
   .then(() => {
     console.log("MongoDB successfully connected");
-    initial();
   })
   .catch(err => console.log(err));
-
-const db = require('./auth/models');
-const Role = db.role;
 
 app.get('/', index.getHomePage);
 app.get('/sort', index.sortFirstNames);
@@ -64,34 +60,3 @@ function listenCallback() {
 }
 
 app.listen(port, hostname, listenCallback);
-
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: 'user'
-      }).save(err => {
-        if (err) {
-          console.log('error', err);
-        }
-        console.log("added 'user' to roles collection");
-      });
-      new Role({
-        name: 'moderator'
-      }).save(err => {
-        if (err) {
-          console.log('error', err);
-        }
-        console.log("added 'moderator' to roles collection");
-      });
-      new Role({
-        name: 'admin'
-      }).save(err => {
-        if (err) {
-          console.log('error', err);
-        }
-        console.log("added 'admin' to roles collection");
-      })
-    }
-  })
-}
