4c6ae594be0c474f7b7719e52295ab65ae522d0d
Tyler <tdiloreto929@gmail.com>
2022-07-18 15:29:37 -0400

    Changed roles to fit ours, simplified roles code

--- auth/middleware/verifySignUp.js
+++ auth/middleware/verifySignUp.js
@@ -30,15 +30,14 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
         });
     });
 };
+//This seems kinda redundant
 checkRolesExisted = (req, res, next) => {
-    if (req.body.roles) {
-        for (let i = 0; i < req.body.roles.length; i++) {
-            if (!ROLES.includes(req.body.roles[i])) {
-                res.status(400).send({
-                    message: `Failed! Role ${req.body.roles[i]} does not exist!`
-                });
-                return;
-            }
+    if (req.body.userType) {
+        if (req.body.userType != 'unregistered' && req.body.userType != 'student' && req.body.userType != 'admin') {
+            res.status(400).send({
+                message: `Failed! Role ${req.body.userType} does not exist!`
+            });
+            return;
         }
     }
     next();
