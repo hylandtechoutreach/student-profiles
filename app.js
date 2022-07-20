4c58cb4f4a1b0240b8b0800b737891c07defaeff
Tyler <tdiloreto929@gmail.com>
2022-07-20 11:16:25 -0400

    Cleaned code, restricted access to pages by role

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const configKeys = require("./config/keys");
+const authJwt = require('./auth/middleware/authJwt');
 const cors = require('cors');
 var corsOptions = {
   origin: 'http://localhost:8081'
@@ -13,6 +14,7 @@ const index = require('./routes/index');
 const program_index = require('./routes/program_index');
 const student = require('./routes/student');
 const program = require('./routes/program');
+const auth = require('./routes/auth');
 
 let app = express();
 
@@ -36,44 +38,27 @@ mongoose.connect(dbStr, dbSettings)
   })
   .catch(err => console.log(err));
 
-app.get('/', index.getHomePage);
-app.get('/sort', index.sortFirstNames);
-app.get('/unsort', index.getHomePage);
-app.get('/add', student.addStudentPage);
-app.get('/edit/:id', student.editStudentPage);
-app.get('/delete/:id', student.deleteStudent);
-app.get('/reactivate/:id', student.reactivateStudent);
-app.get('/next-grade', student.increaseStudentGrades);
-app.get('/filter/:grade', index.filter);
-app.get('/view/:id', student.viewStudentPage);
-app.post('/add', student.addStudent);
-app.post('/edit/:id', student.editStudent);
-
 require('./auth/routes/auth.routes')(app);
-require('./auth/routes/user.routes')(app);
-app.get('/api/auth/signup', (req, res) => {
-  res.render('signup');
-});
-app.get('/api/auth/signin', (req, res) => {
-  res.render('signin');
-});
+app.get('/api/auth/signup', auth.getSignupPage);
+app.get('/api/auth/signin', auth.getSigninPage);
 
-require('./auth/routes/auth.routes')(app);
-require('./auth/routes/user.routes')(app);
-app.get('/api/auth/signup', (req, res) => {
-  res.render('signup');
-});
-app.get('/api/auth/signin', (req, res) => {
-  res.render('signin');
-});
+app.get('/', [authJwt.verifyToken] ,index.getHomePage);
+app.get('/add', [authJwt.verifyToken, authJwt.isStudentOrAdmin], student.addStudentPage);
+app.get('/edit/:id', [authJwt.verifyToken, authJwt.isStudentOrAdmin], student.editStudentPage);
+app.get('/delete/:id', [authJwt.verifyToken, authJwt.isStudentOrAdmin], student.deleteStudent);
+app.get('/reactivate/:id', [authJwt.verifyToken, authJwt.isStudentOrAdmin], student.reactivateStudent);
+app.get('/next-grade', [authJwt.verifyToken, authJwt.isAdmin], student.increaseStudentGrades);
+app.get('/filter/:grade', [authJwt.verifyToken], index.filter);
+app.post('/add', [authJwt.verifyToken, authJwt.isStudentOrAdmin], student.addStudent);
+app.post('/edit/:id', [authJwt.verifyToken, authJwt.isStudentOrAdmin], student.editStudent);
 
-app.get('/program_delete/:id', program.deleteProgram);
-app.get('/program_reactivate/:id', program.reactivateProgram);
-app.get('/program', program_index.getProgramPage);
-app.get('/program_add', program.addProgramPage);
-app.get('/program_edit/:id', program.editProgramPage);
-app.post('/program_add', program.addProgram);
-app.post('/program_edit/:id', program.editProgram);
+app.get('/program_delete/:id', [authJwt.verifyToken, authJwt.isAdmin], program.deleteProgram);
+app.get('/program_reactivate/:id', [authJwt.verifyToken, authJwt.isAdmin], program.reactivateProgram);
+app.get('/program', [authJwt.verifyToken], program_index.getProgramPage);
+app.get('/program_add', [authJwt.verifyToken, authJwt.isAdmin], program.addProgramPage);
+app.get('/program_edit/:id', [authJwt.verifyToken, authJwt.isAdmin], program.editProgramPage);
+app.post('/program_add', [authJwt.verifyToken, authJwt.isAdmin], program.addProgram);
+app.post('/program_edit/:id', [authJwt.verifyToken, authJwt.isAdmin], program.editProgram);
 
 function listenCallback() {
 	console.log(`Server Running on http://${hostname}:${port}`);
