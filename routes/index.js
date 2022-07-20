a46490010f9deb27ffe607d4291dd4d522c17b78
Matt2D3 <matttoodeetoo@gmail.com>
2022-07-20 10:14:56 -0400

    moved some files

--- routes/index.js
+++ routes/index.js
@@ -20,33 +20,6 @@ module.exports = {
 		let renderData = {
 			path: 'none',
 			students: activeStudents
-<<<<<<< HEAD
-		}
-
-		response.render('index', renderData);
-		
-	},
-
-	sortFirstNames: async function(request, response) {
-		let studentList = await db.getStudentsList();
-		let activeStudents = [];
-		for (let i = 0; i < studentList.length; i++) {
-			if (studentList[i].status == "active") {
-				activeStudents.push(studentList[i]);
-			}
-		}
-
-		activeStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
-			ignorePunctuation: true
-		}));
-
-		let renderData = {
-			path: 'none',
-			students: activeStudents
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> baf592c (rebased)
 		}
 
 		response.render('index', renderData);
@@ -99,43 +72,6 @@ module.exports = {
 		let renderData = {
 			path: filteredGrade,
 			students: filteredStudents
-<<<<<<< HEAD
-=======
->>>>>>> 611cb12 (Now always alphabetical, can now search for name)
-		}
-		
-		response.render('index', renderData);
-	},
-
-	filter: async function (request, response) {
-		let studentList = await db.getStudentsList();
-		let filteredGrade = request.params.grade;
-
-		let activeStudents = [];
-		for (let i = 0; i < studentList.length; i++) {
-			if (studentList[i].status == "active") {
-				activeStudents.push(studentList[i]);
-			}
-		}
-
-		let filteredStudents = [];
-		for (let i = 0; i < activeStudents.length; i++) {
-			if (activeStudents[i].grade == filteredGrade) {
-				filteredStudents.push(activeStudents[i]);
-			}
-		}
-
-		filteredStudents.sort( (a, b) => a.first_name.localeCompare(b.first_name, 'fr', {
-			ignorePunctuation: true
-		}));
-
-		let renderData = {
-			path: filteredGrade,
-			students: filteredStudents
-=======
->>>>>>> 4f81e27 (Now always alphabetical, can now search for name)
-=======
->>>>>>> baf592c (rebased)
 		}
 
 		response.render('index', renderData);
